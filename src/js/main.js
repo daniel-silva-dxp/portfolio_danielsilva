0149c7987e8028890a5c7bf344ddc5682b78a2ef
Daniel Silva <dani3l.dxp@gmail.com>
2020-11-10 20:14:56 -0300

    feat: refatorando código

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,4 @@
 "use strict";
-// window.addEventListener("scroll", () => {
-//   let page = this;
-//   let pageTop = this.scrollY;
-//   let pageHeight = this.outerHeight;
-
-//   let frames = document.querySelectorAll(".scrollAnim");
-//   frames.forEach((frame) => {
-//     let frameTop = frame.offsetTop;
-//     let frameHeight = frame.offsetHeight;
-
-//     if (pageTop >= frameTop - pageHeight && pageTop < frameTop + frameHeight) {
-//       frame.classList.add("anim");
-//     } else {
-//       frame.classList.remove("anim");
-//     }
-//   });
-// });
 AOS.init({
   easing: "ease-in-out-sine",
 });
