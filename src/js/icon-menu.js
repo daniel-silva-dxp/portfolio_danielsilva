0f278fbd1a708a466a0c0758533d3f2787b53514
Daniel Silva <dani3l.dxp@gmail.com>
2020-11-10 20:15:49 -0300

    feat: deletando arquivos

--- src/js/icon-menu.js
+++
@@ -1,8 +0,0 @@
-('use strict');
-const menuIcon = document.querySelector('.icon-menu');
-const sidebar = document.querySelector('.sidebar');
-
-menuIcon.addEventListener('click', function() {
-	this.classList.toggle('clicked');
-	sidebar.classList.toggle('open');
-});
