d43a76a1839e5b434ac1fee8aad57bcc156ec422
Daniel Silva <dani3l.dxp@gmail.com>
2020-11-12 10:23:58 -0300

    feat: testando swiper

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,14 @@
 AOS.init({
   easing: "ease-in-out-sine",
 });
+
+var swiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
