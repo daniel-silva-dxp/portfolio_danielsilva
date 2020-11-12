6d30044586b8af16438f1743a88943d7926e94d8
Daniel Silva <dani3l.dxp@gmail.com>
2020-11-12 16:24:26 -0300

    feat: testando envio de form para email

--- src/php/email.php
+++ src/php/email.php
@@ -1,17 +1,17 @@
 <?php
+echo "<script>alert('Mensagem');</script>";
+  // if(isset($_POST['email']) && $_POST['email'] != '') {
 
-  if(isset($_POST['email']) && $_POST['email'] != '') {
-
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $mensagem = $_POST['message'];
+  //   $nome = $_POST['nome'];
+  //   $email = $_POST['email'];
+  //   $mensagem = $_POST['message'];
   
-    $to = 'daniel_dxp@outlook.com';
-    $subject = '[CONTATO] - Website Front-end Developer';
-    $body = "Nome: ".$nome."\r\n"
-            ."E-mail: ".$email."\r\n"
-            ."Mensagem: ".$mensagem;
+  //   $to = 'daniel_dxp@outlook.com';
+  //   $subject = '[CONTATO] - Website Front-end Developer';
+  //   $body = "Nome: ".$nome."\r\n"
+  //           ."E-mail: ".$email."\r\n"
+  //           ."Mensagem: ".$mensagem;
       
-    mail($to,$subject,$body);
-  }
+  //   mail($to,$subject,$body);
+  // }
 ?>
