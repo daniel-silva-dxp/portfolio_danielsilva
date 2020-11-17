4769faef72f5e1ef36d9f3dd6514e9697df5556a
Daniel Silva <dani3l.dxp@gmail.com>
2020-11-16 21:15:25 -0300

    feat: finalizando form contact

--- src/php/contact.php
+++ src/php/contact.php
@@ -1,28 +1,47 @@
 <?php
 
-if(isset($_POST['c_message'])){
+if(isset($_POST['c_email'])){
 
 	$name = $_POST['c_name'];
 	$email = $_POST['c_email'];
 	$message = $_POST['c_message'];
-    
+	$layout = $_POST['c_sim'];
+	 
+	if($layout == '') {
+		$layout = 'Não';
+	}
 	
-	$to      = 'contato@danielsilva-dev.com.br';
+	$to      = "contato@danielsilva-dev.com.br";
 	$subject = 'Site Contact Form';
+	$body = '';
+	$body = "Prezado,\r\n";
+	$body .= "\r\n";
+	$body .= "Meu nome é ".$name ."\r\n";
+	$body .= "Meu e-mail é ".$email ."\r\n";
+	$body .= "Possuo layout? ".$layout ."\r\n";
+	$body .= "Eu tenho uma mensagem pra você, ".$message ."\r\n";
+	$body .= "\r\n";
+	$body .= "Atenciosamente,\r\n";
+	$body .= $name."\r\n";
 
-	$headers = 'From: '. $email . "\r\n" .
-    'Reply-To: '. $email . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+	$headers = "MIME-Version: 1.1\r\n";
+	$headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+	$headers .= "From: contato@danielsilva-dev.com.br\r\n"; // remetente
+	$headers .= "Return-Path: contato@danielsilva-dev.com.br\r\n"; // return-path
 
-	$status = mail($to, $subject, $message, $headers);
+	if(!mail($to, $subject, $body, $headers ,"-r".$to)){ // Se for Postfix
+			$status = mail($to, $subject, $body, $headers);
+	}
+
+	$status = mail($to, $subject, $body, $headers);
 
 	if($status == TRUE){	
-		$res['sendstatus'] = 'done';
+		// $res['sendstatus'] = 'done';
 	
-		//Edit your message here
-		$res['message'] = 'Form Submission Successful';
-    }
-	else{
+		// //Edit your message here
+		// $res['message'] = 'Form Submission Successful';
+		header('location:construct.html');
+  } else{
 		$res['message'] = 'Failed to send mail. Please mail me to daniel_dxp@outlook';
 	}
 	
